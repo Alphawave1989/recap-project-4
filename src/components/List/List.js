f13d545dcfe59089a53428958b077b8bf9f96d5e
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-03-04 09:15:54 +0100

    test

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,25 @@
 import "./List.css";
 
-export default function List({ activities, id }) {
+export default function List({ activities, isGoodWeather, onDeleteActivity }) {
   return (
     <>
+      <h2>
+        {isGoodWeather
+          ? "The weather is awesome! Go outside and:"
+          : "Bad weather outside! Here's what you can do now:"}
+      </h2>
       <ul className="list">
         {activities.map((activity) => (
-          <li key={id} className="item">
+          <li key={activity.id} className="list__item">
             {activity.name}
+            <button
+              type="button"
+              aria-label="delete list item"
+              className="list__item-delete-button"
+              onClick={() => onDeleteActivity(activity.id)}
+            >
+              X
+            </button>
           </li>
         ))}
       </ul>
