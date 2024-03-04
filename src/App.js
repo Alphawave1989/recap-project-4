4d1c977b54549d05f23a5ffd372151fa8e40be98
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-03-04 12:45:53 +0100

    half-way

--- src/App.js
+++ src/App.js
@@ -3,20 +3,54 @@ import Form from "./components/Form/Form.js";
 import useLocalStorageState from "use-local-storage-state";
 import { uid } from "uid";
 import List from "./components/List/List.js";
+import { useEffect, useState } from "react";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
-    defaultValue: [{ id: 2, name: "testName", goodWeather: true }],
+    defaultValue: [{ id: 2, name: "testName" }],
   });
+
+  const [weather, setWeather] = useState([]);
+  console.log(weather);
+
+  const filteredActivities = activities.filter(
+    (activities) => activities.goodWeather === weather.goodWeather
+  );
+
+  useEffect(() => {
+    async function fetchWeather() {
+      const response = await fetch(
+        "https://example-apis.vercel.app/api/weather"
+      );
+      const data = await response.json();
+
+      setWeather(data);
+    }
+
+    fetchWeather();
+  }, [setWeather]);
+
   function handleAddActivity(data) {
     setActivities([{ ...data, id: uid() }, ...activities]);
     console.log(activities);
   }
-
+  if (!weather) return;
   return (
     <div className="box">
-      <List activities={activities} />
-      <Form onAddActivity={handleAddActivity} />
+      {weather === null ? (
+        <h1 className="app-heading">Wetter wird geladen ..</h1>
+      ) : (
+        <>
+          <h1 className="app-heading">
+            <span>{weather.condition}</span>
+          </h1>
+          <List
+            filteredActivities={filteredActivities}
+            goodWeather={weather.goodWeather}
+          />
+          <Form onAddActivity={handleAddActivity} />
+        </>
+      )}
     </div>
   );
 }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,16 @@
 import "./List.css";
 
-export default function List({ activities, id }) {
+export default function List({ filteredActivities, id, goodWeather }) {
   return (
     <>
+      <h2>
+        {goodWeather
+          ? "schönes Wetter! geh raus!"
+          : "schlechtes Wetter! bleib zu Hause"}
+      </h2>
       <ul className="list">
-        {activities.map((activity) => (
-          <li key={id} className="item">
+        {filteredActivities.map((activity) => (
+          <li key={activity.id} className="item">
             {activity.name}
           </li>
         ))}
